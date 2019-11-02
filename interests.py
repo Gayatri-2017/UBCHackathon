83c9946a941fc454ee5a22f8dd42abe5576e47e9
Apple <apple@dhcp-128-189-69-220.ubcsecure.wireless.ubc.ca>
2019-11-02 16:36:43 -0700

    Created interests.py

---
+++ interests.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+# Read data from file 'filename.csv' 
+# (in the same directory that your python process is based)
+# Control delimiters, rows, column names with read_csv (see later) 
+data = pd.read_csv("filename.csv") 
+# Preview the first 5 lines of the loaded data 
+data.head()
